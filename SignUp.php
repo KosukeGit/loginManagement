8232055cb17dce45ad29b21b41519cb0b4f1bb75
uunnee <une327@gmail.com>
2018-04-10 21:26:36 +0900

    modify pdo code

--- SignUp.php
+++ SignUp.php
@@ -29,7 +29,7 @@ if (isset($_POST["signUp"])) {
 		$password = $_POST["password"];
 
 		// 2. ユーザIDとパスワードが入力されていたら認証する
-		$dsn = sprintf('mysql: host=%s; dbname=%s; charset=utf8', $db['host'], $db['dbname']);
+		$dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8', $db['host'], $db['dbname']);
 
 		// 3. エラー処理
 		try {
